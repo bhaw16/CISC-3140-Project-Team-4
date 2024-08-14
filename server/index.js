ac27a98336c6365ee892753dcb47607014dd05d4
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-08-14 15:20:25 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var http = require("http");
+var server = http.createServer().listen(3000);
 
 var { default: mongoose } = require("mongoose");
 var passport = require("passport");
@@ -8,4 +10,9 @@ var localStrategy = require("passport-local");
 var passportLocalMongoose = require("passport-local-mongoose");
 var User = require("model/User");
 
-mongoose.connect("mongodb://localhost/27017");
+mongoose.connect("mongodb://http://127.0.0.1:5500/client");
+
+console.log("Server listening on port 3000");
+
+
+
